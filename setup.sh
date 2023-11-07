c1b5aee73608cf039bc33968019eac312bc3b52d
Lothar Rubusch <lothar.rubusch@enclustra.com>
2023-11-07 19:05:25 +0000

    setup: formatting
    Signed-off-by: Lothar Rubusch <lothar.rubusch@enclustra.com>

--- setup.sh
+++ setup.sh
@@ -11,47 +11,47 @@ die()
 
 apply_cfg_fragment()
 {
-    OPT_RAW="${1}"      ## arg1: config option
-    OPT_SET="$OPT_RAW"
-    CONFIG_FILE="${2}"  ## arg2: config file
+	OPT_RAW="${1}"      ## arg1: config option
+	OPT_SET="$OPT_RAW"
+	CONFIG_FILE="${2}"  ## arg2: config file
 
-    test -z "$OPT_RAW" && return
-    test -z "$OPT_SET" && return
+	test -z "$OPT_RAW" && return
+	test -z "$OPT_SET" && return
 
-    ## option set or disabled?
-    DO_DISABLED="$( echo ${OPT_RAW} | grep "^# " )" || true
-    if [ -n "$DO_DISABLED" ]; then
-        OPT_CONFIG="$( echo $OPT_RAW | awk '{print $2}' )"
-    else
-        OPT_CONFIG="$( echo $OPT_RAW | awk -F'=' '{print $1}' )"
-    fi
-
-    ## replace or append option?
-    DO_REPLACE="$( grep "$OPT_CONFIG" -r $CONFIG_FILE )" || true
-    if [ -n "$DO_REPLACE" ]; then
-        sed -i "\|${OPT_CONFIG}|s|.*|${OPT_SET}|"  "$CONFIG_FILE" &> /dev/null
-        grep $OPT_CONFIG -HIrn --color $CONFIG_FILE
-    else
-        echo "$OPT_SET" >> "$CONFIG_FILE"
-    fi
+	## option set or disabled?
+	DO_DISABLED="$( echo ${OPT_RAW} | grep "^# " )" || true
+	if [ -n "$DO_DISABLED" ]; then
+		OPT_CONFIG="$( echo $OPT_RAW | awk '{print $2}' )"
+	else
+		OPT_CONFIG="$( echo $OPT_RAW | awk -F'=' '{print $1}' )"
+	fi
+	
+	## replace or append option?
+	DO_REPLACE="$( grep "$OPT_CONFIG" -r $CONFIG_FILE )" || true
+	if [ -n "$DO_REPLACE" ]; then
+		sed -i "\|${OPT_CONFIG}|s|.*|${OPT_SET}|"  "$CONFIG_FILE" &> /dev/null
+		grep $OPT_CONFIG -HIrn --color $CONFIG_FILE
+	else
+		echo "$OPT_SET" >> "$CONFIG_FILE"
+	fi
 }
 
 identify_machine_parent()
 {
-    ## MACHINE base type, needed for initial petalinux setup
-    case ${MACHINE} in
-	*-zx1-*) ;&
-	*-zx2-*) ;&
-	*-zx3-*)  MACHINE_PARENT_TYPE="zynq" ;;
-	*-xu1-*) ;&
-	*-xu3-*) ;&
-	*-xu5-*) ;&
-	*-xu6-*) ;&
-        *-xu6cg-*) ;&
-	*-xu8-*) ;&
-	*-xu9-*) MACHINE_PARENT_TYPE="zynqMP" ;;
-        *) die "setup.sh: MACHINE_PARENT_TYPE: MACHINE '$MACHINE' cannot be parsed" ;;
-    esac
+	## MACHINE base type, needed for initial petalinux setup
+	case ${MACHINE} in
+		*-zx1-*) ;&
+		*-zx2-*) ;&
+		*-zx3-*)  MACHINE_PARENT_TYPE="zynq" ;;
+		*-xu1-*) ;&
+		*-xu3-*) ;&
+		*-xu5-*) ;&
+		*-xu6-*) ;&
+		*-xu6cg-*) ;&
+		*-xu8-*) ;&
+		*-xu9-*) MACHINE_PARENT_TYPE="zynqMP" ;;
+		*) die "setup.sh: MACHINE_PARENT_TYPE: MACHINE '$MACHINE' cannot be parsed" ;;
+	esac
 }
 
 ## MAIN
@@ -95,17 +95,17 @@ identify_machine_parent
 ## NB: for the read-approach the .cfg file must have a final empty line/EOF, if not the last option will be omitted (fix this?)
 cd "${PETALINUXDIR}"
 if [ -e "./enclustra/${MACHINE_PARENT_TYPE}/petalinux-${BOOTMODE}.cfg" ]; then
-    OLDIFS="$IFS"
-    IFS=$'\n'
-    while read line; do
-        CONFIG_PETALINUX=( ${CONFIG_PETALINUX[*]} "$line" )
-    done < ./enclustra/${MACHINE_PARENT_TYPE}/petalinux-${BOOTMODE}.cfg ## NEVER use quotes here!
-    IFS="$OLDIFS"
+	OLDIFS="$IFS"
+	IFS=$'\n'
+	while read line; do
+		CONFIG_PETALINUX=( ${CONFIG_PETALINUX[*]} "$line" )
+	done < ./enclustra/${MACHINE_PARENT_TYPE}/petalinux-${BOOTMODE}.cfg ## NEVER use quotes here!
+	IFS="$OLDIFS"
 fi
 
 ## petalinux-config - apply configs
 for ((idx = 0; idx < ${#CONFIG_PETALINUX[@]}; idx++)); do
-    apply_cfg_fragment "${CONFIG_PETALINUX[$idx]}" "./project-spec/configs/config"
+	apply_cfg_fragment "${CONFIG_PETALINUX[$idx]}" "./project-spec/configs/config"
 done
 
 ## rootfs-config - basics go here
@@ -115,17 +115,17 @@ CONFIG_ROOTFS=(
 ## rootfs-config - read and append config fragments according to boot mode
 cd "${PETALINUXDIR}"
 if [ -e "./enclustra/${MACHINE_PARENT_TYPE}/rootfs-${BOOTMODE}.cfg" ]; then
-    OLDIFS="$IFS"
-    IFS=$'\n'
-    while read line; do
-        CONFIG_ROOTFS=( ${CONFIG_ROOTFS[*]} "$line" )
-    done < ./enclustra/${MACHINE_PARENT_TYPE}/petalinux-${BOOTMODE}.cfg ## NEVER use quotes here!
-    IFS="$OLDIFS"
+	OLDIFS="$IFS"
+	IFS=$'\n'
+	while read line; do
+		CONFIG_ROOTFS=( ${CONFIG_ROOTFS[*]} "$line" )
+	done < ./enclustra/${MACHINE_PARENT_TYPE}/petalinux-${BOOTMODE}.cfg ## NEVER use quotes here!
+	IFS="$OLDIFS"
 fi
 
 ## rootfs-config - apply configs
 for ((idx = 0; idx < ${#CONFIG_ROOTFS[@]}; idx++)); do
-    apply_cfg_fragment "${CONFIG_ROOTFS[$idx]}" "./project-spec/configs/rootfs_config"
+	apply_cfg_fragment "${CONFIG_ROOTFS[$idx]}" "./project-spec/configs/rootfs_config"
 done
 
 ## fix: provide MACHINE for petalinux setup (has to be in .conf file
@@ -135,7 +135,7 @@ MACHINEOVERRIDE="enclustra-${BOOTMODE}"
 echo "MACHINEOVERRIDES =. \"${MACHINEOVERRIDE}:\"" > ./build/conf/enclustra.inc
 
 if [ -z "$(grep "require conf/enclustra.inc" -r ./build/conf/local.conf)" ]; then
-    echo "require conf/enclustra.inc" >> ./build/conf/local.conf
+	echo "require conf/enclustra.inc" >> ./build/conf/local.conf
 fi
 
 ## fix: remove petalinux warning about locked signatures,
@@ -148,7 +148,7 @@ cd "${PETALINUXDIR}"
 petalinux-config --silentconfig
 
 if [ -z "$( grep "include conf/petalinuxbsp.conf" -r ./build/conf/local.conf )" ]; then
-    echo "include conf/petalinuxbsp.conf" >> ./build/conf/local.conf
+	echo "include conf/petalinuxbsp.conf" >> ./build/conf/local.conf
 fi
 
 rm -v ./setup.sh
