69e1bf7720520b7a1a3deddcd49f29f741ced09a
Arthur Ruder <arthur.ruder@enclustra.com>
2024-09-26 09:30:14 +0200

    Use one rootfs config per boot mode mainly because of QSPI which would not fit into the 32 MBytes set aside for kernel+rootfs

--- setup.sh
+++ setup.sh
@@ -113,12 +113,12 @@ CONFIG_ROOTFS=(
 ## rootfs-config - read and append config fragments according to boot mode
 echo "======================================= Apply petalinux rootfs configs ========================================"
 cd "${PETALINUXDIR}"
-if [ -e "./enclustra/${MACHINE_PARENT_TYPE}/rootfs.cfg" ]; then
+if [ -e "./enclustra/common/rootfs-${BOOTMODE}.cfg" ]; then
 	OLDIFS="$IFS"
 	IFS=$'\n'
 	while read line; do
 		CONFIG_ROOTFS=( ${CONFIG_ROOTFS[*]} "$line" )
-	done < ./enclustra/${MACHINE_PARENT_TYPE}/rootfs.cfg ## NEVER use quotes here!
+	done < ./enclustra/common/rootfs-${BOOTMODE}.cfg ## NEVER use quotes here!
 	IFS="$OLDIFS"
 fi
 
